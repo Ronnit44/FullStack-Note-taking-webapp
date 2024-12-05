d23181b0767454246c2f7ad575eb44b5e40a352f
Ronnit44 <redmironit32@gmail.com>
2024-12-05 19:13:47 +0530

    added eye button in password field

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,20 @@
-import Form from "../components/form"
-function Register(){
-    return <Form route="/api/user/register/" method="register" />
+import React from "react";
+import Form from "../components/form";
+import { Link } from "react-router-dom"; 
+
+function Register() {
+  return (
+    <div>
+      <Form route="/api/user/register/" method="register" />
+      
+      <div style={{ marginTop: "20px", textAlign: "center" }}>
+        <h2>
+          Already have an account?{" "}
+          <Link to="/login" className="register-link">Login here</Link>
+        </h2>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
