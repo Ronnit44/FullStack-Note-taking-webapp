0fc49c534d2c8099ada0ef1c03d711ff37dcaff8
Ronnit44 <redmironit32@gmail.com>
2025-02-11 17:28:19 +0530

    fixed login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,3 @@
-// import Form from "../components/form"
-
-// function Login() {
-//     return <Form route="/api/token/" method="login" />
-// }
-
-// export default Login
 import React from "react";
 import { Link } from "react-router-dom";
 import Form from "../components/form"; 
